97a29cc0bb5debff5136e61931d2c51c141e3519
vaishnavi <vaishnavirashankar18@gmail.com>
2024-03-19 15:29:38 +0530

    Changes in Booklet

--- src/app/ArticleCardsPage/page.tsx
+++ src/app/ArticleCardsPage/page.tsx
@@ -15,35 +15,35 @@ const ArticlePage = () => {
 
     const matches = useMediaQuery("(min-width:600px)");
 
-    
+
 
     return (
         <Box sx={{ backgroundColor: "#FEFCEA" }}>
             <Typography variant='h6' p={2} fontSize="small">
                 <b> Home /</b> Articles
             </Typography>
-          
+
             <FilterBar />
             <Box display="flex" justifyContent="center" gap={5} flexWrap="wrap" >
                 <Box display="flex" flexDirection="column">
-                    <Section title={''}  />
-                    <Section  title={''}  />
-                    <Section  title={''}  />
-                    <Section  title={''}  />
+                    <Section title={''} />
+                    <Section title={''} />
+                    <Section title={''} />
+                    <Section title={''} />
                 </Box>
                 <Box display="flex" flexDirection="column" gap={10} >
                     <TopicTwo />
                     <RESVisitedArticle />
                 </Box>
             </Box>
-           
+
         </Box>
     );
 };
 
 const Section = ({ title }: { title: string }) => (
     <Box>
-        <Typography variant="h6" sx={{ fontWeight: "bold", justifyContent:'flex-start' ,mr:5}}>
+        <Typography variant="h6" sx={{ fontWeight: "bold", justifyContent: 'flex-start', mr: 5 }}>
             {title}
         </Typography>
         <Box display="flex" justifyContent="center" gap={5} flexWrap="wrap" m={2}>
--- src/app/components/BookletCard.tsx
+++ src/app/components/BookletCard.tsx
@@ -9,51 +9,51 @@ function BookCard() {
         router.push('/BookDetails');
     };
     return (
-      <Box sx={{ width: "230px" }}>
-          <Card
-              onClick={handleClickBookDetails}
-              sx={{ maxWidth: '300px', mb: 2, backgroundColor: '#FCDFA0', alignItems: 'center', borderRadius: '15px', border: "1px solid" }}
-          >
-              <Box sx={{ display: "flex", justifyContent: "center", backgroundColor: "white" }}>
-                  <CardMedia
-                      loading="lazy"
-                      component="img"
-                      image="/book2.jpg"
-                      sx={{ backgroundColor: "white", height: "140px", width: "50%" }}
-                      alt="Shrimad Bhaghwatgita"
-                  />
-              </Box>
-              <CardContent>
-                  <Typography sx={{ fontWeight: "bold", justifyContent: 'flex-start', ml: '-0.5em' }}>
-                      Bhagvad Gita-As it is
-                  </Typography>
-                  <Typography variant="h6" sx={{ fontSize: "10px", color: "gray", ml: '-0.6em'}}>
-                      By Srila Prabhupad
-                  </Typography>
-              </CardContent>
-              <Stack direction="row" justifyContent="space-between" alignItems="flex-end" p={1} mt={'-1.5em'}>
-                  <Typography variant="h6" sx={{ fontSize: "10px", color: "gray" }}>
-                      12 Jan 2024
-                  </Typography>
-                  <Button
-                      onClick={handleClickBookDetails}
-                      sx={{
-                          fontWeight: "bold",
-                          border: "1px solid black",
-                          color: "white",
-                          fontSize: "10px",
-                          backgroundColor: "#81311A",
-                          borderRadius: 10,
-                          height: "25px",
-                          cursor: "pointer",
-                      }}
-                  >
-                      Buy
-                  </Button>
-              </Stack>
-          </Card>
-      </Box>
-  );
+        <Box sx={{ width: "230px" }}>
+            <Card
+                onClick={handleClickBookDetails}
+                sx={{ maxWidth: '300px', mb: 2, backgroundColor: '#FCDFA0', alignItems: 'center', borderRadius: '15px', border: "1px solid" }}
+            >
+                <Box sx={{ display: "flex", justifyContent: "center", backgroundColor: "white" }}>
+                    <CardMedia
+                        loading="lazy"
+                        component="img"
+                        image="/book2.jpg"
+                        sx={{ backgroundColor: "white", height: "140px", width: "50%" }}
+                        alt="Shrimad Bhaghwatgita"
+                    />
+                </Box>
+                <CardContent>
+                    <Typography sx={{ fontWeight: "bold", justifyContent: 'flex-start', ml: '-0.5em' }}>
+                        Bhagvad Gita-As it is
+                    </Typography>
+                    <Typography variant="h6" sx={{ fontSize: "10px", color: "gray", ml: '-0.6em' }}>
+                        By Srila Prabhupad
+                    </Typography>
+                </CardContent>
+                <Stack direction="row" justifyContent="space-between" alignItems="flex-end" p={1} mt={'-1.5em'}>
+                    <Typography variant="h6" sx={{ fontSize: "10px", color: "gray" }}>
+                        12 Jan 2024
+                    </Typography>
+                    <Button
+                        onClick={handleClickBookDetails}
+                        sx={{
+                            fontWeight: "bold",
+                            border: "1px solid black",
+                            color: "white",
+                            fontSize: "10px",
+                            backgroundColor: "#81311A",
+                            borderRadius: 10,
+                            height: "25px",
+                            cursor: "pointer",
+                        }}
+                    >
+                        Buy
+                    </Button>
+                </Stack>
+            </Card>
+        </Box>
+    );
 };
 
 export default BookCard;
